a73c8db1f54b3c06d20a92241378747a80985541
Frida <fridapihlstrom@gmail.com>
2026-05-20 11:31:13 +0200

    Skapat CourseService för att hämta JSON-data

--- src/app/course-list/course-list.ts
+++ src/app/course-list/course-list.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CourseService } from
 
 @Component({
   selector: 'app-course-list',
---
+++ src/app/services/course.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CourseServices {
+  private url: string = 'https://webbutveckling.miun.se/files/ramschema.json';
+  constructor(private http: HttpClient) { }
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
+}
