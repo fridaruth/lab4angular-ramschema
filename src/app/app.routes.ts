41f7732737f2692a48858dd930e2d5948e925db3
Frida <fridapihlstrom@gmail.com>
2026-05-20 11:11:32 +0200

    Lagt till CourseList component och routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { CourseList } from './course-list/course-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: CourseList },
+    { path: "", redirectTo: "/home", pathMatch: "full" }
+];
